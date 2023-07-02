3eaa0a94cc065be42f06b9e1ef58c4273631a3fd
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-02 15:52:37 +0000

    Restore the nine files to where found
    	new file:   sketch-dd/diagram.json
    	new file:   sketch-dd/fool.c
    	new file:   sketch-dd/n_sketch-dd.cpp
    	new file:   sketch-dd/note_b.md
    	new file:   sketch-dd/sketch-dd.ino
    	new file:   sketch-dd/src/forth.c
    	new file:   sketch-dd/src/forth.h
    	new file:   sketch-dd/src/tty_io.c
    	new file:   sketch-dd/wokwi-project.txt

---
+++ compile-test.d/test-sketches.d/sketch-dd/n_sketch-dd.cpp
@@ -0,0 +1,100 @@
+#include <Arduino.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// void experiment_a_asm(unsigned int *rptr);
+
+// extern void this_here();
+
+void this_here();
+void interpreter(void);
+
+/*
+char getch(void);
+void putch(char c);
+int getquery(void);
+*/
+
+
+extern void this_here_now();
+
+char getch(void) {
+//  this_here_now();
+    bool waiting_ch = 0;
+
+    for (int testing = 5; testing > 0; testing--) {
+      ;
+    }
+
+    while (!waiting_ch) {
+      waiting_ch = Serial.available();
+      // this_here_now(); // loop
+    }
+
+    if (waiting_ch) {
+      char ch = Serial.read();
+      return ch;
+    }
+}
+
+void putch(char c) {
+    printf("%c", c);
+
+    return ;  // doesn't have to do anything
+}
+
+
+int getquery(void) {
+    // return(UARTCharsAvail(UART0_BASE) != 0);
+    return(0 != 0);
+}
+
+
+
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+
+
+void trapped() {
+  // this_here_now();
+  // this_here_now();
+
+  interpreter();
+
+  while(-1);
+}
+
+void setup() {
+  Serial.begin(9600);
+  Serial.write(' ');
+  Serial.println("\n\n Camelforth in C");
+  pinMode(15, OUTPUT);
+
+/*
+  for (int count = 3; count > 0; count--) {
+    digitalWrite(15, 1);
+    delay(90);
+    digitalWrite(15, 0);
+    delay(700);
+  }
+*/
+
+  // this_here_now();
+
+  // pinMode(LED_BUILTIN, OUTPUT);
+
+  trapped();
+}
+
+void loop() {
+  ;
+}
+
+// END.
