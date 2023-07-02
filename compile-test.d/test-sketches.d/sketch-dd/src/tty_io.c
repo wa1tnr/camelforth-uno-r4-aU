f61d4aa6c15d574cfe7d1622922d901781562fe8
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-02 12:40:06 +0000

    placeholder functions terminal io

---
+++ compile-test.d/test-sketches.d/sketch-dd/src/tty_io.c
@@ -0,0 +1,14 @@
+
+char getch(void) {
+    char space = ' ';
+    return space;
+}
+
+void putch(char c) {
+    return ;  // doesn't have to do anything
+}
+
+int getquery(void) {
+    // return(UARTCharsAvail(UART0_BASE) != 0);
+    return(0 == 0);
+}
