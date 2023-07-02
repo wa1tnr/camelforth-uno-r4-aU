b0d760e88e7fa69c633e74b0b60268ff0d6da866
wa1tnr <wa1tnr@users.noreply.github.com>
2023-07-02 11:47:45 +0000

    small sketch that blinks LED
      possibly adhering to better practices,
      wrt .c source inclusion
    
    	modified:   fool.c
    
    On branch post-hold-aa

--- compile-test.d/test-sketches.d/sketch-cc/fool.c
+++ compile-test.d/test-sketches.d/sketch-cc/fool.c
@@ -1,10 +1,42 @@
 /* begin */
 
-void this_here() {
+
+int state ;
+
+void toggle_LED(int pinNumber) {
+  state = !state;
+  digitalWrite(pinNumber, state);
+}
+
+void slowest() {
+  for (volatile int iter = 9800; iter > 1; iter--) {
+    ;
+  }
+}
+
+void slower() {
+  for (volatile int repeating = 25;
+  repeating > 0; repeating--) {
+    slowest();
+  }
+}
+
+void this_here_now() {
+  toggle_LED(7);
+  slower();
+  toggle_LED(7);
+  for (int off_to_on_ratio = 7; off_to_on_ratio > 1; off_to_on_ratio--) {
+    slower();
+  }
   int test = 0;
   for (test = 22; test > 0; test--) {
     ;
   }
 }
 
+void this_here() {
+  while(-1) {
+    this_here_now(); // loop
+  }
+}
 /* end */
