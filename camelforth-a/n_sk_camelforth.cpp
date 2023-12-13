326e8bbbac321cb6f237c1605a0b77bb93c7e25d
wa1tnr <wa1tnr@users.noreply.github.com>
2023-12-13 02:41:26 +0000

    cleanup

--- camelforth-a/n_sk_camelforth.cpp
+++ camelforth-a/n_sk_camelforth.cpp
@@ -1,107 +1,67 @@
-/* Sun  2 Jul 16:18:33 UTC 2023 */
+/* Tue 12 Dec 23:34:02 UTC 2023 */
 #include <Arduino.h>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-/* functions in a .c file are NOT given as extern prototype */
-/* just do the usual prototype without the extern qualifier */
-void this_here();
 void interpreter(void);
-
-void printStuff(char* buffer);
-
-/*
-char getch(void);
-void putch(char c);
-int getquery(void);
-*/
-
-
-extern void this_here_now();
+void printStuff(char *buffer);
 
 char getch(void) {
-    bool waiting_ch = 0;
-    for (int testing = 5; testing > 0; testing--) {
-      ;
-    }
+  bool waiting_ch = 0;
+  for (int testing = 5; testing > 0; testing--) {
+    ;
+  }
 
-    while (!waiting_ch) {
-      waiting_ch = Serial.available();
-    }
+  while (!waiting_ch) {
+    waiting_ch = Serial.available();
+  }
 
-    if (waiting_ch) {
-      char ch = Serial.read();
-      return ch;
-    }
+  if (waiting_ch) {
+    char ch = Serial.read();
+    return ch;
+  }
 }
 
-void putLine(char* buffer) {
-    Serial.print(buffer);
-}
+void putLine(char *buffer) { Serial.print(buffer); }
 
 void putch(char c) {
-    // printf("%c", c);
-    Serial.print(c);
-
-    return ;  // doesn't have to do anything
+  Serial.print(c);
+  return; // doesn't have to do anything
 }
 
-
-int getquery(void) {
-    // return(UARTCharsAvail(UART0_BASE) != 0);
-    return(0 != 0);
-}
-
-
-
-
-
+int getquery(void) { return (0 != 0); }
 
 #ifdef __cplusplus
 }
 #endif
 
-
-
 void trapped() {
   interpreter();
-
-  Serial.println("");
-  Serial.println("");
-  Serial.println(" Calling code found in:"); Serial.println("");
-  Serial.println("");
-  Serial.println("     camelforth-esp32-aU/camelforth-a/n_sk_camelforth.cpp");
-  Serial.println("");
-  Serial.println("");
-  Serial.println(" hey guess what: someone asked for 'BYE' and you got it: (endless loop)");
-  while(-1);
+  Serial.println("stale print");
+  while (-1)
+    ;
 }
 
-void blynque() {
-    for (int count = 3; count > 0; count--) {
-        digitalWrite(13, 1);
-        delay(1000);
-        digitalWrite(13, 0);
-        delay(1000);
-        delay(1000);
-    }
+void setup_serial() {
+  Serial.begin(115200);
+  while (!Serial)
+    ;
 }
+
+void setup_gpio() { pinMode(13, OUTPUT); }
 
 void setup() {
-  Serial.begin(115200);
-  while(!Serial);
-  pinMode(13, OUTPUT);
-  // blynque();
+  setup_serial();
+  setup_gpio();
+
   delay(800);
   Serial.write(' ');
   Serial.println("\n\n Camelforth in C");
   trapped();
 }
 
-void loop() {
-  ;
-}
+void loop() { ; }
 
 // END.
